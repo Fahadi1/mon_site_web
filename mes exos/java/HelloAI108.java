a754c45bd5d96b0e1edd53af01dfae819022f1ec
Floriane <floriane.ahadi@gmail.com>
2020-10-22 16:42:11 +0200

    je suppr le dossier exos

--- mes exos/java/HelloAI108.java
+++
@@ -1,7 +0,0 @@
-// la classe est le nom de mon fichier .java
-public class HelloAI108{
-	//la méthode main initialise mon programme
-	public static void main(String[] arg){
-		System.out.println("Hello AI108 !");
-		}		 
-}
